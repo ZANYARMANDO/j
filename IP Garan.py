e924217d5e280833e21410ff944dcebe96cefbaa
ZANYARMANDO <zanyarmando1999@gmail.com>
2016-07-05 08:39:55 +0100

    Add files via upload

---
+++ IP Garan.py
@@ -0,0 +1,14 @@
+from socket import gethostbyname
+
+def Miscript():
+	print'\a=[baxyrybyn bo barnamay IP garan drwst krawa lalayan ZANYAR MANDO]='
+	target = raw_input('./gulm aw hostay athawy bynwsa ba bow fa3s bkam: ')
+	targetIP = gethostbyname(target)
+	print '\a-Target IP ===>', targetIP
+	print '+------------------------------------+'
+	Miscript()
+Miscript()
+
+
+
+# bydzy awdzm 
